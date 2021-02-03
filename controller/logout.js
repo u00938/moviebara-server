885aeaf18b66b515dec9bba195150f062c096250
u00938 <yo01030184262@gmail.com>
2021-02-03 16:45:38 +0900

    add controller

---
+++ controller/logout.js
@@ -0,0 +1,8 @@
+// const { user } = require('../models')
+
+module.exports = {
+  post: (req, res) => {
+    res.send('no')
+
+  }
+}
---
+++ controller/scraps.js
@@ -0,0 +1,13 @@
+const { scrap, post } = require('../models')
+
+module.exports = {
+  post: (req, res) => {
+
+  },
+  getScrapById: (req, res) => {
+
+  },
+  deleteScrap: (req, res) => {
+    
+  }
+}
