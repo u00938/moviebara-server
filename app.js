39cd06fa305249ff363e9b25a918f2523f235b13
MUNA <munawiki@gmail.com>
2021-02-04 00:29:47 +0900

    배포를 위한 환경설정

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const bodyparser = require('body-parser');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyparser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// CLI에서 export NODE_ENV='development' 실행하고 작업해주세요
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: path.join(__dirname, ".env.production") });
+} else if (process.env.NODE_ENV === "development") {
+  dotenv.config({ path: path.join(__dirname, ".env.development") });
+} else {
+  throw new Error("process.env.NODE_ENV를 설정하지 않았습니다.");
+}
+
 const port = process.env.PORT || 4000;
 
 const app = express();
 
-const loginRouter = require('./routes/login')
-const logoutRouter = require('./routes/logout')
-const usersRouter = require('./routes/users')
-const moviesRouter = require('./routes/movies')
-const postsRouter = require('./routes/posts')
-const scrapsRouter = require('./routes/scraps');
+const loginRouter = require("./routes/login");
+const logoutRouter = require("./routes/logout");
+const usersRouter = require("./routes/users");
+const moviesRouter = require("./routes/movies");
+const postsRouter = require("./routes/posts");
+const scrapsRouter = require("./routes/scraps");
 
+app.use("/login", loginRouter);
+app.use("/logout", logoutRouter);
+app.use("/users", usersRouter);
+app.use("/movies", moviesRouter);
+app.use("/posts", postsRouter);
+app.use("/scraps", scrapsRouter);
 
-app.use('/login', loginRouter)
-app.use('/logout', logoutRouter)
-app.use('/users', usersRouter)
-app.use('/movies', moviesRouter)
-app.use('/posts', postsRouter)
-app.use('/scraps', scrapsRouter)
+app.use(logger("dev"));
 
-app.use(logger('dev'));
-
-app.use(cors({
+app.use(
+  cors({
     origin: true,
-    methods: 'GET, POST, PATCH, DELETE, OPTIONS',
-    credentials: true
-}))
+    methods: "GET, POST, PATCH, DELETE, OPTIONS",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(bodyparser.json());
 
-app.listen(port, () => console.log("hello"))
+app.listen(port, () => console.log("hello"));
