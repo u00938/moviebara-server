ff542e7bee65c38b19982fb7069dc31bf9d574e7
u00938 <yo01030184262@gmail.com>
2021-02-04 15:56:16 +0900

    issue22

--- controller/movies.js
+++ controller/movies.js
@@ -1,12 +1,27 @@
-// const { movie } = require('../models')
+const { movie } = require('../models')
 
 module.exports = {
-  get: (req, res) => {
-    res.send('no')
-
+  get: async (req, res) => {
+    try {
+      if (req.query) {
+        const query = req.query
+        if (query.movie_title) {
+          const movieByTitle = await movie.findOne({ where: { title: query.movie_title } })
+          if (movieByTitle) res.json(movieByTitle)
+          else res.status(404).json({ message: "Couldn't find this movie." })
+        }
+        if (query.movie_id) {
+          const movieById = await movie.findOne({ where: { id: query.movie_id } })
+          if (movieById) res.json(movieById)
+          else res.status(404).json({ message: "Couldn't find this movie." })
+        }
+      }
+      else {
+        const movies = await movie.findAll();
+        res.status(200).json({ ok: movies })
+      }
+    } catch(err) {
+      console.log(err)
+    }
   },
-  getMovieById: (req, res) => {
-    res.send('no')
-
-  }
 }
