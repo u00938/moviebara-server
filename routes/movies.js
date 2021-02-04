505026c8e79ad8b89590751bef979d05c0e7f4ff
u00938 <yo01030184262@gmail.com>
2021-02-04 15:52:24 +0900

    update movies route

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const controller = require('../controller/movies')
 
 router.get('/', controller.get)
-router.get('/:movie_id', controller.getMovieById)
 
 module.exports = router;
