70356443e2e3fd4566894ed798c3ae162c4fa187
power100413 <power100413@naver.com>
2021-02-12 17:31:27 +0900

    fix_login

--- controller/login.js
+++ controller/login.js
@@ -9,7 +9,7 @@ module.exports = {
     const { email, password } = req.body;
     if(!email || !password) {
       // 정보가 더 필요하다면 에러메시지
-      res.status(400).json({ message: "should send full data" })
+      res.status(400).json({ data: null, message: "should send full data" })
     }
     // body에 담긴 정보로 유저정보를 불러온다.
     const userInfo = await user.findOne({
@@ -19,7 +19,8 @@ module.exports = {
     if(!isSame) {
       // 정보가 유효하지 않을 경우, 에러메시지
       res.status(400).json({
-        "message":"go away! I don't know you "
+        data: null,
+        message : "go away! I don't know you "
       })
     } else {
       // 액세스 토큰 발급
